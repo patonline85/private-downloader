caa2035fccb673b801665befcb5a185d9a12696d
patonline85 <77493414+patonline85@users.noreply.github.com>
2025-12-29 17:27:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -339,5 +339,4 @@ async def get_file(filename: str):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=7000)
-
+    uvicorn.run(app, host='0.0.0.0', port=8000)
